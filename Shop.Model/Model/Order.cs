105de14d7a424b7f8240b647780bf07b206d49db
binh <vuontoidinhcao6789@gmail.com>
2016-07-13 23:21:59 +0700

    check code fix bug

---
+++ Shop.Model/Model/Order.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shop.Model.Model
+{
+   [Table("Orders")]
+    public class Order
+    {
+       [Key]
+       [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+       [Required]
+       [MaxLength(255)]
+       public string CustomerName { get; set; }
+       [Required]
+       [MaxLength(500)]
+       public string CustomerAddress { get; set; }
+
+       [Required]
+       [MaxLength(300)]
+       public string CustomerEmail { get; set; }
+       [Required]
+       [MaxLength(50)]
+       public string CustomerMobile { set; get; }
+
+       [Required]
+       [MaxLength(256)]
+       public string CustomerMessage { set; get; }
+
+       [MaxLength(256)]
+       public string PaymentMethod { set; get; }
+
+       public DateTime? CreatedDate { set; get; }
+       public string CreatedBy { set; get; }
+       public string PaymentStatus { set; get; }
+       public bool Status { set; get; }
+       public virtual IEnumerable<OrderDetail> OrderDetails { set; get; }
+    }
+}
